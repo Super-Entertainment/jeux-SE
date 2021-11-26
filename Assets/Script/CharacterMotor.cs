ab38bf7fa9e44f4e2df051f727e641c0a28f181a
Starflashexe <starflash4000@gmail.com>
2021-11-26 19:40:12 +0100

    CONTROLE PERSONNAGE
    on peut marcher mais c est pas optimisé

---
+++ Assets/Script/CharacterMotor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour
+{
+
+    // Animation du perso
+    Animation animations;
+
+    // Vitesse de déplacement
+    public float walkSpeed;
+    public float runSpeed;
+    public float turnSpeed;
+
+    //Inputs
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+
+    public Vector3 jumpSpeed;
+    CapsuleCollider playerCollider;
+
+    void Start()
+    {
+        animations = gameObject.GetComponent<Animation>();
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckCapsule(playerCollider.bounds.center, new Vector3(playerCollider.bounds.center.x, playerCollider.bounds.min.y - 0.1f, playerCollider.center.z), 0.08f);
+    }
+    void Update()
+    {
+        //si on avance 
+        if (Input.GetKey(inputFront) && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, walkSpeed * Time.deltaTime);
+            animations.Play("walk");
+        }
+
+        //si on sprint
+        if (Input.GetKey(inputFront) && Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
+            animations.Play("run");
+        }
+
+        //si on recule
+        if (Input.GetKey(inputBack))
+        {
+            transform.Translate(0, 0, -(walkSpeed / 2) * Time.deltaTime);
+            animations.Play("walk");
+        }
+
+        //rotation gauche
+        if (Input.GetKey(inputLeft))
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+        //rotation droite
+        if (Input.GetKey(inputRight))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+        //si on fait rien
+        if (!Input.GetKey(inputFront) && !Input.GetKey(inputBack))
+        {
+            animations.Play("idle");
+        }
+        //si on saute
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            // Préparation du saut
+            Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+            v.y = jumpSpeed.y;
+
+            //saut
+            gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+        }
+    }
+}
